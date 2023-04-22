220c2f6ee496ee24876fafb4443448b76ceda1a4
MiguelAndrade866 <a31366@tomazpelayo.com>
2023-04-22 17:30:47 +0100

    exercicio15

--- LerComGetchar.c
+++ LerComGetchar.c
@@ -1,3 +1,11 @@
-// Resolva neste ficheiro o seu exercício
+#include <stdio.h>
+int main ()
+{
+char letra;
+printf("Escreva uma letra no terminal e pressione a tecla Enter:  ");
+letra = getc(stdin);
+printf("A letra que escreveu foi: %c\n", letra);
+return 0;
+}
 
 
